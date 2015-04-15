9951ced1fc88f3d2f3e0cf67deb84fbc311b8eac
richmondwatkins <richmondwatkins@mac.com>
2015-04-14 20:18:54 -0500

    removed double from form class

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,22 +1,4 @@
 class Form
   include Mongoid::Document
 
-  # field :location, :type => Hash
-
-  field :name, :type => Array
-
-  field :slug, :type => String
-
-  field :address, :type => String
-
-  field :phoneNumber, :type => String
-
-  field :website, :type => String
-
-  field :dealDays, :type => Array
-
-  field :coords, :type => Hash
-
-  field :rating, :type => Double
-  
 end
